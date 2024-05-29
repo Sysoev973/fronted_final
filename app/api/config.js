ad918bae622a84f4c63a3a44f2b50b8599cd5d99
Sysoev973 <149953815+Sysoev973@users.noreply.github.com>
2024-05-29 14:43:19 +0300

    last review

--- app/api/config.js
+++ app/api/config.js
@@ -1,8 +1,8 @@
-export const BASE_URL = 'https://sysoev-backend.nomoredomainswork.ru/api' 
+export const BASE_URL = 'https://sysoev-backend.nomorepartiesco.ru/api' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/api/auth/login`,
-  me: `${BASE_URL}/api/me`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
 }
 //d
